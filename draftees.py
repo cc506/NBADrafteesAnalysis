abd48c19361b25170060c514c24752e7796e26ef
cc506 <cchung28@ucmerced.edu>
2018-11-21 13:27:25 -0800

    test stuff

--- draftees.py
+++ draftees.py
@@ -1,6 +1,14 @@
 import json
+import requests
 
 player = ''
+isCBB = None
+
+def checkPlayer():
+    global player
+    global isCBB
+
+    
 
 def check():
     htmlData = requests.get('https://www.sports-reference.com/cbb/players/'+ player + '-1.html')
