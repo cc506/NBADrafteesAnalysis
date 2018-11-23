595518e0cb0e448a1cda2bc376035afd2de63112
cc506 <cchung28@ucmerced.edu>
2018-11-22 23:30:08 -0800

    Scrapped First Table

--- draftees.py
+++ draftees.py
@@ -1,14 +1,17 @@
-import json
 import requests
+import json
+from bs4 import BeautifulSoup
 
-player = ''
+player = 'deandre-ayton'
 isCBB = None
+htmlData = requests.get('https://www.sports-reference.com/cbb/players/'+ player + '-1.html')
 
-def checkPlayer():
-    global player
-    global isCBB
+soup = BeautifulSoup(htmlData.text, 'lxml')
 
-    
+ #This is the index of any table of that page. If you change it you can get different tables.
+table = soup.find_all('table')[0] 
+tab_data = [[celldata.text for celldata in rowdata.find_all(["th","td"])]
+                        for rowdata in table.find_all("tr")]
 
-def check():
-    htmlData = requests.get('https://www.sports-reference.com/cbb/players/'+ player + '-1.html')
+for data in tab_data:
+     print(' '.join(data))
